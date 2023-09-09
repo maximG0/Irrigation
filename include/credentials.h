b83190fc0a951e36a1060dddb06261a853a04e6a
Max9764 <jajub37@gmail.com>
2023-09-09 10:44:09 +0300

    Moved credentials.h

---
+++ include/credentials.h
@@ -0,0 +1,5 @@
+#define BLYNK_TEMPLATE_ID "TMPL6L41e2dPb"
+#define BLYNK_TEMPLATE_NAME "Irrigation"
+#define BLYNK_AUTH_TOKEN "5eny6l6r4v4awRNb6T-vxLruEyMp9t3c"
+char ssid[] = "PartnerFiber";
+char pass[] = "0778840447";
